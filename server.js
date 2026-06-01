cd4395b3f9a9684baee3e7ba8889321b8d9d4b90
doreyaaazayed <doreyazayed4@gmail.com>
2026-06-01 03:19:21 +0300

    hi

--- server.js
+++ server.js
@@ -47,34 +47,13 @@ const { isGoogleConfigured, oauthRedirectUri, getFrontendUrl } = require("./serv
 const app = express();
 
 // Middleware (raise body limit so event creation with base64 photo can succeed)
-const allow_origins = [
-  "http://localhost:5173/",
-];
 
-//cors policies
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-
-      if (allow_origins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  }),
-);
+app.use(cors());
 app.use(compression());
 app.use(express.json({ limit: "15mb" }));
 
 // Event hero images (written by eventImageService on create/update)
-app.use("/", (req, res) => {
-  res.json({
-    msg: "done",
-  });
-});
+
 
 app.use(
   "/api/uploads/events",
@@ -120,6 +99,11 @@ app.use("/api/loyalty", loyaltyRoutes);
 // Health check route
 app.get("/api/health", (req, res) => {
   res.json({ status: "ok" });
+});
+app.use("/", (req, res) => {
+  res.json({
+    msg: "done",
+  });
 });
 
 const PORT = process.env.PORT || 5000;
